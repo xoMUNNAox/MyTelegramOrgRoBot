2a1f3b42ac0f99e85ade5b074e3a0a347f9b7bb6
xoMUNNAox <83388044+xoMUNNAox@users.noreply.github.com>
2021-04-29 20:12:54 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -13,7 +13,7 @@ This code is only used for the purpose of getting the APP ID from my.telegram.or
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SEDxD"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
     IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
     IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
