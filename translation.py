d5bbc7985c950a05c50dd59d53aa2bcd66244841
xoMUNNAox <83388044+xoMUNNAox@users.noreply.github.com>
2021-04-29 18:46:30 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,6 @@
 class Translation(object):
     START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
+please read the TnC before proceeding: https://t.me/X_F0RCED/279
 Thank you for using me 😬
 Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
 
@@ -8,9 +8,8 @@ Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
     AFTER_RECVD_CODE_TEXT = """I see!
 now please send the Telegram code that you received from Telegram!
 
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
+This code is only used for the purpose of getting the APP ID from my.telegram.org
+
 
 /start at any stage to re-enter your details"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
